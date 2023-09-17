a3920a55ddb49e5679db8be1fafceff93e4ef154
Gabriel Leite <gabrielcurvelo@gmail.com>
2023-09-17 11:54:21 -0300

    refactor ♻️: refactored

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ This is a GitHub action to bump a given semantic version, depending on a given v
 
 **Required** The current semantic version you want to increment. (e.g. 3.12.5)
 
-### `version-fragment`
+### `release-type`
 
 **Required** The versions fragment you want to increment.
 
-Possible options are **[ major | feature | bug | alpha | beta | pre | rc ]**
+Possible options are **[ major | feature | fix | refactor | docs ]**
 
 ## Outputs
 
-### `next-version`
+### `new-version`
 
 The incremented version.
 
@@ -24,40 +24,24 @@ The incremented version.
 
     - name: Bump release version
       id: bump_version
-      uses: christian-draeger/increment-semantic-version@1.1.0
+      uses: gabclincrement-semantic-version@1.1.0
       with:
-        current-version: '2.11.7-alpha.3' # also accepted: 'v2.11.7-alpha.3' | '2.11.7-alpha3'
-        version-fragment: 'feature'
+        current-version: '2.11.7' # also accepted: 'v2.11.7'
+        release-type: 'feature'
     - name: Do something with your bumped release version
       run: echo ${{ steps.bump_version.outputs.next-version }}
       # will print 2.12.0
       
 ## input / output Examples
 
-| version-fragment | current-version |   | output         |
-| ---------------- | --------------- | - | -------------- |
-| major            | 2.11.7          |   | 3.0.0          |
-| major            | v2.11.7         |   | 3.0.0          |
-| major            | 2.11.7-alpha3   |   | 3.0.0          |
-| major            | 2.11.7-alpha.3  |   | 3.0.0          |
-| feature          | 2.11.7          |   | 2.12.0         |
-| feature          | 2.11.7-alpha3   |   | 2.12.0         |
-| feature          | 2.11.7-alpha.3  |   | 2.12.0         |
-| bug              | 2.11.7          |   | 2.11.8         |
-| bug              | 2.11.7-alpha3   |   | 2.11.8         |
-| bug              | 2.11.7-alpha.3  |   | 2.11.8         |
-| alpha            | 2.11.7          |   | 2.11.7-alpha.1 |
-| alpha            | 2.11.7-alpha3   |   | 2.11.7-alpha.4 |
-| alpha            | 2.11.7-alpha.3  |   | 2.11.7-alpha.4 |
-| beta             | 2.11.7          |   | 2.11.7-beta.1  |
-| beta             | 2.11.7-alpha3   |   | 2.11.7-beta.1  |
-| beta             | 2.11.7-alpha.3  |   | 2.11.7-beta.1  |
-| pre              | 2.11.7          |   | 2.11.7-pre.1   |
-| pre              | 2.11.7-alpha3   |   | 2.11.7-pre.1   |
-| pre              | 2.11.7-alpha.3  |   | 2.11.7-pre.1   |
-| rc               | 2.11.7          |   | 2.11.7-rc.1    |
-| rc               | 2.11.7-alpha3   |   | 2.11.7-rc.1    |
-| rc               | 2.11.7-alpha.3  |   | 2.11.7-rc.1    |
+| release-type | current-version |     | output |
+| ------------ | --------------- | --- | ------ |
+| major        | 2.11.7          |     | 3.0.0  |
+| major        | v2.11.7         |     | 3.0.0  |
+| feature      | 2.11.7          |     | 2.12.0 |
+| fix          | 2.11.7          |     | 2.11.8 |
+| refactor     | 2.11.7          |     | 2.11.8 |
+| docs         | 2.11.7          |     | 2.11.8 |
 
 # License
 The scripts and documentation in this project are released under the [MIT License](LICENSE)
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,95 +11,60 @@ set -euo pipefail
 
 main() {
 
-  prev_version="$1"; release_type="$2"
+    # Retrieve input parameters
+    CURRENT_VERSION="$1"; RELEASE_TYPE="$2"
 
-  if [[ "$prev_version" == "" ]]; then
-    echo "could not read previous version"; exit 1
-  fi
+    # PARAMETER CHECKS ---------------------------
 
-  possible_release_types="major feature bug alpha beta pre rc"
+    # Possible release types
+    #   possible_release_types="major feature bug alpha beta pre rc"
+    POSSIBLE_RELEASE_TYPES="major feature fix refactor docs"
 
-  if [[ ! ${possible_release_types[*]} =~ ${release_type} ]]; then
-    echo "valid argument: [ ${possible_release_types[*]} ]"; exit 1
-  fi
-
-  major=0; minor=0; patch=0; pre=""; preversion=0
-
-  # break down the version number into it's components
-  regex="^v?([0-9]+).([0-9]+).([0-9]+)((-[a-z]+).?([0-9]+))?$"
-  if [[ $prev_version =~ $regex ]]; then
-    major="${BASH_REMATCH[1]}"
-    minor="${BASH_REMATCH[2]}"
-    patch="${BASH_REMATCH[3]}"
-    pre="${BASH_REMATCH[5]}"
-    preversion="${BASH_REMATCH[6]}"
-  else
-    echo "previous version '$prev_version' is not a semantic version"
-    exit 1
-  fi
-
-  # increment version number based on given release type
-  case "$release_type" in
-  "major")
-    ((++major)); minor=0; patch=0; pre="";;
-  "feature")
-    ((++minor)); patch=0; pre="";;
-  "bug")
-    ((++patch)); pre="";;
-  "alpha")
-    if [[ -z "$preversion" ]];
-      then
-        preversion=0
-      else
-        if [[ "$pre" != "-alpha" ]];
-          then
-          preversion=1
-          else ((++preversion))
-        fi
+    # Check for CURRENT_VERSION
+    if [[ "$CURRENT_VERSION" == "" ]]; then
+        echo "could not read previous version"; exit 1
     fi
-    pre="-alpha.$preversion";;
-  "beta")
-    if [[ -z "$preversion" ]];
-      then
-        preversion=0
-      else
-        if [[ "$pre" != "-beta" ]];
-          then
-          preversion=1
-          else ((++preversion))
-        fi
-    fi
-    pre="-beta.$preversion";;
-  "pre")
-    if [[ -z "$preversion" ]];
-      then
-        preversion=0
-      else
-        if [[ "$pre" != "-pre" ]];
-          then
-          preversion=1
-          else ((++preversion))
-        fi
-    fi
-    pre="-pre.$preversion";;
-  "rc")
-    if [[ -z "$preversion" ]];
-      then
-        preversion=0
-      else
-        if [[ "$pre" != "-rc" ]];
-          then
-          preversion=1
-          else ((++preversion))
-        fi
-    fi
-    pre="-rc.$preversion";;
-  esac
 
-  next_version="${major}.${minor}.${patch}${pre}"
-  echo "create $release_type-release version: $prev_version -> $next_version"
+    # Check for RELEASE_TYPE
+    if [[ ! ${POSSIBLE_RELEASE_TYPES[*]} =~ ${RELEASE_TYPE} ]]; then
+        echo "valid argument: [ ${POSSIBLE_RELEASE_TYPES[*]} ]"; exit 1
+    fi
 
-  echo "next-version=$next_version" >> $GITHUB_OUTPUT
+    # Break down CURRENT_VERSION ---------------------------
+    # Initialize variables
+    major=0; minor=0; patch=0;
+
+    # break down the version number into it's components
+    regex="^v?([0-9]+).([0-9]+).([0-9]+)((-[a-z]+).?([0-9]+))?$"
+    if [[ $CURRENT_VERSION =~ $regex ]]; then
+        major="${BASH_REMATCH[1]}"
+        minor="${BASH_REMATCH[2]}"
+        patch="${BASH_REMATCH[3]}"
+    else
+        echo "previous version '$CURRENT_VERSION' is not a semantic version"
+        exit 1
+    fi
+
+    # CALCULATE NEW VERSION ---------------------------
+    # increment version number based on given release type
+    case "$RELEASE_TYPE" in
+    "major")
+        ((++major)); minor=0; patch=0;;
+    "feature")
+        ((++minor)); patch=0;;
+    "fix")
+        ((++patch));;
+    "refactor")
+        ((++patch));;
+    "docs")
+        ((++patch));;
+    esac
+
+    NEW_VERSION="${major}.${minor}.${patch}"
+    echo "Create $RELEASE_TYPE-release version: $CURRENT_VERSION -> $NEW_VERSION"
+
+    echo "new-version=$NEW_VERSION" >> $GITHUB_OUTPUT
 }
 
+# Run
 main "$1" "$2"
